03853f45e73e4df028af4ec14ec2750524139f81
Lucas Barros Bezerra <lucasbabezerra@gmail.com>
2021-11-07 21:42:26 -0300

    correcao nos testes que haviam quebrado com a paerte de seguranca

--- src/test/java/br/com/alura/carteira/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/alura/carteira/controller/UsuarioControllerTest.java
@@ -1,21 +1,29 @@
 package br.com.alura.carteira.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
+import br.com.alura.carteira.infra.security.TokenService;
+import br.com.alura.carteira.modelo.Perfil;
+import br.com.alura.carteira.modelo.Usuario;
+import br.com.alura.carteira.repository.PerfilRepository;
+import br.com.alura.carteira.repository.UsuarioRepository;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -25,33 +33,46 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 class UsuarioControllerTest {
 
 	@Autowired
-	//Simula requisições http para teste
+	// Simula requisições http para teste
 	private MockMvc mvc;
-	
+
+	@Autowired
+	private TokenService tokenService;
+
+	@Autowired
+	private PerfilRepository perfilRepository;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	private String token;
+
+	@BeforeEach
+	public void gerarToken() {
+		Usuario logado = new Usuario("Rodrigo", "rodrigo", "123456");
+		Perfil admin = perfilRepository.findById(1l).get();
+		logado.adicionarPerfil(admin);
+		usuarioRepository.save(logado);
+		Authentication authentication = new UsernamePasswordAuthenticationToken(logado, logado.getLogin());
+		this.token = tokenService.gerarToken(authentication);
+	}
+
+	@Test
+	void naodeveriaCadastrarUsuarioComDadosInCompletos() throws Exception {
+		String json = "{}";
+
+		mvc.perform(post("/usuarios").contentType(MediaType.APPLICATION_JSON).content(json).header("Authorization",
+				"Bearer " + token)).andExpect(status().isBadRequest());
+	}
+
 	@Test
 	void deveriaCadastrarUsuarioComDadosCompletos() throws Exception {
-		String json = "{}";
-		
-		mvc
-		.perform
-		(post("/usuarios")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(json))
-				.andExpect(status().isBadRequest());
-	}
-	
-	@Test
-	void naoDeveriaCadastrarUsuarioComDadosIncompletos() throws Exception {
-		String json = "{\"nome\" : \"fulano\", \"login\":\"fulano@email.com\"}";
-		
-		mvc
-		.perform
-		(post("/usuarios")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(json))
-				.andExpect(status().isCreated())
-				.andExpect(header().exists("Location"))
-				.andExpect(content().json(json));
+		String json = "{\"nome\" : \"fulano\", \"login\":\"fulano@email.com\", \"perfilId\" :1}";
+		String jsonEsperado = "{\"nome\" : \"fulano\", \"login\":\"fulano@email.com\"}";
+
+		mvc.perform(post("/usuarios").contentType(MediaType.APPLICATION_JSON).content(json).header("Authorization",
+				"Bearer " + token)).andExpect(status().isCreated()).andExpect(header().exists("Location"))
+				.andExpect(content().json(jsonEsperado));
 	}
 
 }
--- src/test/java/br/com/alura/carteira/service/TransacaoServiceTest.java
+++ src/test/java/br/com/alura/carteira/service/TransacaoServiceTest.java
@@ -1,23 +1,27 @@
 package br.com.alura.carteira.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 
 import br.com.alura.carteira.dto.TransacaoDto;
 import br.com.alura.carteira.dto.TransacaoFormDto;
 import br.com.alura.carteira.modelo.TipoTransacao;
+import br.com.alura.carteira.modelo.Transacao;
+import br.com.alura.carteira.modelo.Usuario;
 import br.com.alura.carteira.repository.TransacaoRepository;
 import br.com.alura.carteira.repository.UsuarioRepository;
 
@@ -29,9 +33,19 @@ class TransacaoServiceTest {
 
 	@Mock
 	private UsuarioRepository usuarioRepository;
+	
+	@Mock
+	private ModelMapper modelMapper;
 
 	@InjectMocks
 	private TransacaoService service;
+
+	private Usuario logado;
+
+	@BeforeEach
+	public void before() {
+		this.logado = new Usuario("Rodrigo", "Rodrigo@email.com", "123456");
+	}
 
 	private TransacaoFormDto criarTransacaoFormDto() {
 		TransacaoFormDto formDto = new TransacaoFormDto("ITSA4", new BigDecimal("10.24"), LocalDate.now(), 120,
@@ -43,8 +57,33 @@ class TransacaoServiceTest {
 	void deveriaCadastrarUmaTransacao() {
 
 		TransacaoFormDto formDto = criarTransacaoFormDto();
+		
+		Mockito
+			.when(usuarioRepository.getById(formDto.getUsuarioId()))
+			.thenReturn(logado);
+		
+		Transacao transacao = new Transacao(formDto.getTicker(),
+				formDto.getPreco(),
+				formDto.getQuantidade(),
+				formDto.getData(),
+				formDto.getTipo(), logado);
+		
+		Mockito
+			.when(modelMapper.map(transacao, TransacaoDto.class))
+			.thenReturn(new TransacaoDto(
+					null,
+					transacao.getTicker(),
+					transacao.getPreco(),
+					transacao.getQuantidade(),
+					transacao.getTipo()
+					));
+		
+		Mockito
+		.when(modelMapper.map(formDto, Transacao.class))
+		.thenReturn(transacao);
 
-		TransacaoDto dto = service.cadastrar(formDto);
+
+		TransacaoDto dto = service.cadastrar(formDto, logado);
 
 		Mockito.verify(repository).save(Mockito.any());
 
@@ -62,7 +101,7 @@ class TransacaoServiceTest {
 
 		Mockito.when(usuarioRepository.getById(formDto.getUsuarioId())).thenThrow(EntityNotFoundException.class);
 
-		assertThrows(IllegalArgumentException.class, () -> service.cadastrar(formDto));
+		assertThrows(IllegalArgumentException.class, () -> service.cadastrar(formDto, logado));
 
 	}
 
